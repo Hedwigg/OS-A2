0cc27c2edc2e101a5d27a9e45bd7ea6c80f5cc24
Joel Lechman <jlechman@universalathletic.com>
2018-10-17 09:25:35 -0600

    Initial file creation.

---
+++ main.c
@@ -0,0 +1,66 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+
+  /*Linked List of Nodes*/
+
+
+
+  /*Node object*/
+  struct Node  {
+	int data;
+	struct Node* next;
+	struct Node* prev;
+};
+
+//Creates a new Node and returns pointer to it.
+struct Node* GetNewNode(int x) {
+	struct Node* newNode
+		= (struct Node*)malloc(sizeof(struct Node));
+	newNode->data = x;
+	newNode->prev = NULL;
+	newNode->next = NULL;
+	return newNode;
+}
+
+//Inserts a Node at head of doubly linked list
+void InsertAtHead(int x) {
+	struct Node* newNode = GetNewNode(x);
+	if(head == NULL) {
+		head = newNode;
+		return;
+	}
+	head->prev = newNode;
+	newNode->next = head;
+	head = newNode;
+
+
+}
+
+//Prints all the elements in linked list in forward traversal order
+void Print() {
+	struct Node* temp = head;
+	printf("Forward: ");
+	while(temp != NULL) {
+		printf("%d ",temp->data);
+		temp = temp->next;
+	}
+	printf("\n");
+}
+
+
+int main()
+{
+
+}
