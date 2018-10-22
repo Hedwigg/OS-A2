ec686a903e5c3b713fc13efc7ac8efb810bb74ac
Joel Lechman <jlechman@universalathletic.com>
2018-10-22 13:54:42 -0600

    Update main.c
    Threads working. Need to finish up mutex handling and the individual logic for each thread.

--- main.c
+++ main.c
@@ -15,7 +15,13 @@ struct Node
 struct Node *head;		//pointer to head of D-LL
 int maxBufferSize = 30;
 int currentBufferSize = 1;
-bool BufferStatus = 0; // 0 = not full, 1 = buffer full.
+bool BufferFull = 0;
+bool BufferEmpty = 0;
+
+pthread_mutex_t lock; //mutex lock for threads
+
+
+
 
 //Creates a new Node and returns pointer to it. (helper function for InsertAtEnd)
 struct Node *
@@ -60,69 +66,74 @@ void PrintBuffer ()
   printf ("\n");
 }
 
+//Buffer full message
+void bufferFullMessage()
+{
+    printf("buffer is full");
+}
+
+//buffer empty message
+void bufferEmptyMessage()
+{
+    printf("buffer is empty");   
+}
+
 /*
 Producer # 1 will generate a node and add it at the end of the linked list, the
 value of the new node is a random ODD integer less than 50.
-*/
-void P1 ()
-{
-    printf("in p1 \n");
-    printf("%d", currentBufferSize);
-    printf("\n");
-    PrintBuffer();
 
-    if(BufferStatus == 1) //if buffer is full
-    {
-      //generate a full message and wait.
-
-    }
-    else//if buffer is not full
-    {
-        int r;
-        while(r % 2 == 1)
-        {
-            printf("buffer not full: producing a node : ");
-            r = rand() % 50;
-            printf("%d", r);
-        }
-        //int r = rand () % 50; //generate random ODD int < 50s
-        InsertAtEnd (r);
-        printf("not full");
-        if(currentBufferSize > 29)
-        {
-            BufferStatus = 1;
-        }
-    }
-
-}
-
-/*
 Producer #2 will generate a node and add it at the end of the linked list, the
 value of the new node is a random EVEN integer less than 50.
-*/
-void P2 ()
-{
 
-}
-
-/*
 Consumer #1 will delete, from the ehad of the list, the first node whos value is ODD
 (if the first node has an even value, then wait). When the buffer is empty, both should
 generate a message and wait.
-*/
-void C1 ()
-{
 
-}
-
-/*
 Consumer # 2 will delete, from the head of the list, the first node whos value is even
 (if the first node has an odd value, then wait). When the buffer is empty, both should
 generate a message and wait.
 */
-
-void C2 ()
+void *produceOrConsume(void *tID_point)
 {
+    int *tID = (int *)tID_point;
+    
+    printf("in a thread");
+    printf("%d ---" , *tID);
+    
+    
+    pthread_mutex_lock(&lock); //lock
+
+    if(*tID == 1)  //Producer 1 (odd)
+    {
+        //printf("\nin 1\n ");
+        
+        //https://stackoverflow.com/questions/13675132/pthread-cond-wait-for-2-threads
+        
+        if(BufferFull == 1) 
+        {
+         bufferFullMessage();  
+         pthread_cond_wait(&BufferFull,&lock);
+        }
+        
+    }else if(*tID ==2) //Producer 2 (even)
+    {
+        //printf("\nin 2\n ");
+   
+    }else if(*tID ==3) //Consumer 1 (odd)
+    {
+        // printf("\nin 3\n ");
+
+        
+    }else if(*tID ==4) //Consumer 2 (even)
+    {
+        //printf("\nin 4\n ");
+
+    }else
+    {
+        printf("error in produceOrConsume");
+    }
+    
+    pthread_mutex_unlock(&lock); //unlock
 
 }
 
@@ -144,8 +155,29 @@ int main ()
   printf ("Initial buffer-");
   PrintBuffer ();
 
-  //Threads
-  P1(); //not in the form of a thread for testing
+    //Threads
+    pthread_t thread1; //Producer 1
+    pthread_t thread2; //Producer 2
+    pthread_t thread3; //Consumer 1
+    pthread_t thread4; //Consumer 2
+    int P1 = 1;
+    int P2 = 2;
+    int C1 = 3;
+    int C2 = 4;
+    
+    pthread_create(&thread1, NULL, produceOrConsume, &P1);
+    pthread_create(&thread2, NULL, produceOrConsume, &P2);
+    pthread_create(&thread3, NULL, produceOrConsume, &C1);
+    pthread_create(&thread4, NULL, produceOrConsume, &C2);
+
+//Join threads after finished.
+    pthread_join(thread1, NULL);
+    pthread_join(thread2, NULL);
+    pthread_join(thread3, NULL);
+    pthread_join(thread4, NULL);
 
 
+    
 }
+
+
